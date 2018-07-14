6778f1ec6b8eb08ac7310eb5782384fcae6ddf07
Colin Milhaupt <colinmilhaupt@gmail.com>
2018-07-14 13:57:12 -0600

    Made results print in alphabetical order by last name

--- generate.py
+++ generate.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 import random
 import sys
 
@@ -19,22 +21,27 @@ def printGDS(self):
 
 def getStudentByNumber(num):
     for student in students:
-        if int(student.tid) == num:
-            return student.name
+        if student.tid == num:
+            return student
     return -1;
 
 class Student:
     def __init__(self, name, role, num):
         self.name = name
         fullname = name.split(' ')
+        self.first = fullname[0]
         self.last = fullname[1]
-        self.tid = num
+        self.tid = int(num)
         if role == 'g':
             gds.append(self)
-        else:
+        elif role == 'c':
+            constants.append(self)
+        elif role == 'm':
             students.append(self)
-            if role == 'l' or role == 'r':
-                constants.append(self)
+        else:
+            print 'Incorrect marking for student', name
+            print 'Please correct and run again.'
+            print 'Exiting...'
 
 
 for line in open('rosters/tmp-master-roster.txt'):
@@ -44,12 +51,6 @@ for line in open('rosters/tmp-master-roster.txt'):
         s = Student(person[1], person[0], -1)
     if len(person) == 3:
         s = Student(person[1], person[0], person[2])
-
-student_set = set(students)
-constant_set = set(constants)
-
-toRandomize = student_set - constant_set
-randomize = list(toRandomize)
 
 print '/------------------------------\\'
 print '| GDS:                         |'
@@ -62,18 +63,24 @@ for g in gds:
     string += '|'
     print string
 print '\------------------------------/'
+print ''
+print 'Assignments in alphabetical order'
+print '----------------------------------'
 
 for i in range(0, random.randint(0, 100)):
-    random.shuffle(randomize)
+    random.shuffle(students)
+
+for i in range(len(constants)):
+    students.append(constants[i])
 
 j = 0
-
 for i in range(10, 70):
-    s = getStudentByNumber(i)
-    if s == -1:
-        sys.stdout.write('T')
-        print i, '-', randomize[j].name
+    if getStudentByNumber(i) == -1:
+        students[j].tid = i
         j += 1
-    else:
-        sys.stdout.write('T')
-        print i, '-', s
+
+students.sort(key=lambda x: x.last)
+
+for student in students:
+    sys.stdout.write(student.last + ", " + student.first + ": ")
+    print "T" + str(student.tid)
